45485ed87606baffa744626a0b745242f92eb016
chrisegener22 <78327583+chrisegener22@users.noreply.github.com>
2024-02-21 15:08:19 -0500

    more changes

--- hireup/src/env.js
+++ hireup/src/env.js
@@ -7,16 +7,8 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z
-      .string()
-      .url()
-      .refine(
-        (str) => str.includes("mysql://8y2fcre2zggp199nezn3:pscale_pw_kmobfP8qJ09ERldyu3lqxrzhw7BAnD7jJFM475Dpc8z@aws.connect.psdb.cloud/hireup?sslaccept=strict"),
-        
-      ),
-    NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
+    DATABASE_URL: z.string().url(), // Simply validating if it's a URL
+    NODE_ENV: z.enum(["development", "test", "production"]).optional(), // Making it optional, will use process.env.NODE_ENV if not provided
   },
 
   /**
@@ -34,14 +26,16 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
+    NODE_ENV: process.env.NODE_ENV || 'development', // Default to 'development' if not set
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
+
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
    * useful for Docker builds.
    */
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+
   /**
    * Makes it so that empty strings are treated as undefined.
    * `SOME_VAR: z.string()` and `SOME_VAR=''` will throw an error.
