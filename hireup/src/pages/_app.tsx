dff44a0b6297ddad208dedcaf2e779a91918ced0
Benjamin Falkner <bfalkner9@gmail.com>
2024-02-21 14:40:06 -0500

    added auth

--- hireup/src/pages/_app.tsx
+++ hireup/src/pages/_app.tsx
@@ -4,8 +4,14 @@ import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
 
+import { UserProvider } from '@auth0/nextjs-auth0/client';
+
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return( 
+    <UserProvider >
+    <Component {...pageProps} />
+  </UserProvider>
+    ) 
 };
 
 export default api.withTRPC(MyApp);
