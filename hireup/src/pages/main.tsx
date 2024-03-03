707bae646725fc94497986042f0b4a6a7888be92
Benjamin Falkner <bfalkner9@gmail.com>
2024-03-02 23:42:33 -0500

    added grid and event card

--- hireup/src/pages/main.tsx
+++ hireup/src/pages/main.tsx
@@ -1,12 +1,16 @@
-import { BentoGrid } from "~/components/bento-grid";
+import EventCard from "~/components/EventCard";
 
 
 export default function Main() {
-
+    const eventCards = Array.from({ length: 10 }, (_, index) => <div className="mx-40 my-5">
+    <EventCard key={index}/>
+</div>);
     return (
-      <>
-       <BentoGrid/>
-      </>
+        <div className="flex flex-wrap gap-4 mt-12">
+      {eventCards}
+    </div>
+        
+        
     );
   }
   
