350e8b32066a028e1fa4cc8cdadedfea645aed8a
chrisegener22 <78327583+chrisegener22@users.noreply.github.com>
2024-03-15 15:34:25 -0400

    small changes to main

--- hireup/src/pages/main.tsx
+++ hireup/src/pages/main.tsx
@@ -5,7 +5,7 @@ import { BackgroundBeams } from "~/components/ui/background-beams";
 
 export default function Main() {
   return (
-    <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+    <div className="h-screen w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased mt-[-100px]">
       <div className="max-w-2xl mx-auto p-4">
         <h1 className="relative z-10 text-lg md:text-7xl  bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600  text-center font-sans font-bold ">
           Join the waitlist
